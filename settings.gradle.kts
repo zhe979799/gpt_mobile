6e51175150898c6ec5702b253fca9d3226dfcba0
zhz <zhaohaozhe@exiao.tech>
2025-03-17 10:01:25 +0800

    feat: init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,24 @@
+
+// settings.gradle.kts
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }  // 阿里云镜像
+        maven { url = uri("https://maven.aliyun.com/repository/google") }  // Google 镜像
+        maven { url = uri("https://jitpack.io") }
         gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
         google()
         mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)  // 强制优先使用 settings 仓库
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://jitpack.io") }
+        google()
+        mavenCentral()
     }
 }
 
